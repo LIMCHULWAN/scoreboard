e80b41d1da38164dd36f7604abb0bc03ff04e388
kosmo_10 <limchulwan132@gmail.com>
2019-06-22 12:55:51 +0900

    컴포넌트를 모듈 분리 완료

--- src/App.js
+++ src/App.js
@@ -1,50 +1,8 @@
 import React from 'react';
 import {Header} from './components/Header';
 import './App.css';
+import {Player} from "./components/Player";
 
-const Player = (props) => (
-    <div className="player">
-        <span className="player-name">
-            <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-          {props.name}
-        </span>
-      <Counter score={props.score} />
-    </div>
-)
-
-class Counter extends React.Component{
-
-  constructor(){
-    super();
-    this.state = {
-      score:0,
-      time:10
-    };
-    // this.incrementScore = this.incrementScore.bind(this);
-  }
-
-  // 애로우펑션안의 this는 lexical this로써 자기자신을 가리킨다.
-  handleScore = (delta) =>{
-    console.log('increment:', this);
-
-    // this.state.score += 1;
-    // setState를 호출해야한 UI 렌더링이 된다.
-    this.setState({score: this.state.score + delta});
-    this.setState(prevState =>{
-      return {score:prevState.score}
-    })
-  }
-
-  render(){
-    return (
-        <div className="counter">
-          <button className="counter-action decrement" onClick={() => this.handleScore(-1)}> - </button>
-          <span className="counter-score">{this.state.score}</span>
-          <button className="counter-action increment" onClick={() => this.handleScore(1)}> + </button>
-        </div>
-    )
-  }
-}
 
 class App extends React.Component{
   state = {
---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export class Counter extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            score:0,
+            time:10
+        };
+        // this.incrementScore = this.incrementScore.bind(this);
+    }
+
+    // 애로우펑션안의 this는 lexical this로써 자기자신을 가리킨다.
+    handleScore = (delta) =>{
+        console.log('increment:', this);
+
+        // this.state.score += 1;
+        // setState를 호출해야한 UI 렌더링이 된다.
+        this.setState({score: this.state.score + delta});
+        this.setState(prevState =>{
+            return {score:prevState.score}
+        })
+    }
+
+    render(){
+        return (
+            <div className="counter">
+                <button className="counter-action decrement" onClick={() => this.handleScore(-1)}> - </button>
+                <span className="counter-score">{this.state.score}</span>
+                <button className="counter-action increment" onClick={() => this.handleScore(1)}> + </button>
+            </div>
+        )
+    }
+}
