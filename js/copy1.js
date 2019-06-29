cd946fffc37f2eb15e43762c4fd2d922bfb307fa
kosmo_10 <limchulwan132@gmail.com>
2019-06-29 09:41:27 +0900

    Deep copy

--- js/copy1.js
+++ js/copy1.js
@@ -7,9 +7,13 @@ let book = {title: "인사이드 자바스크립트", price: 18000};
 
 // 2. book을 카피하되 deep copy를 수행한 후에 price를 30000으로 바꾸고 출력하라. 두개의 값이 달라야 한다.
 // 2-1 Object.assign() 사용하여 deep copy하기
-let copyBook = Object.assign({book})
+// let copyBook = Object.assign([],book,{a:1});
+// copyBook.price = 20000;
+// console.log(book);
+// console.log(copyBook);
+
+// 2-2 es6 spread 연산자를 사용하여 deep copy 하기
+let copyBook = {...book, ...{b:1}};
 copyBook.price = 20000;
 console.log(book);
 console.log(copyBook);
-
-// 2-2 es6 spread 연산자를 사용하여 deep copy 하기
