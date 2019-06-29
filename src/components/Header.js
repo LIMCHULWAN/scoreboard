2a0f1f13388e2adc6e0d36dc6e7159ca6812c0ca
kosmo_10 <limchulwan132@gmail.com>
2019-06-29 11:13:35 +0900

    1. ref and createRef()로 DOM노드로 접근 2. propTypes로 타입 체크, defaultProp 설정하기, static 키워드

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Stats} from "./Stats";
 import {Stopwatch} from "./Stopwatch";
+import PropTypes from 'prop-types';
 
-export const Header = (props) => {
+export const Header = ({title,players}) => {
     // console.log(props);
-    const {title, totalPlayers, players} = props;    // destruct assignment
+    // const {title, totalPlayers, players} = props;    // destruct assignment
     return (
         <header className="header">
             <Stats players={players}/>
@@ -12,4 +13,18 @@ export const Header = (props) => {
             <Stopwatch/>
         </header>
     )
+}
+
+Header.propTypes = {
+    title:PropTypes.string,
+    players:PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        score: PropTypes.number,
+        name: PropTypes.string
+    }))
+}
+
+Header.defaultProps = {
+    title:'Scoreboard'
+
 }
