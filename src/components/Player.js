26b6ad255fba6ab2815418eecd14a678fecc9ed8
kosmo_10 <limchulwan132@gmail.com>
2019-06-29 10:03:46 +0900

    PureComponent를 이용해서 원하는 컴포넌트만 렌더링하기

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-
-export const Player = (props) => (
-    <div className="player">
+export class Player extends React.PureComponent{
+    render(){
+        console.log(this.props.name, ' rendered');
+        return(
+            <div className="player">
         <span className="player-name">
-            <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-            {props.name}
+            <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
+            {this.props.name}
         </span>
-        <Counter score={props.score} id={props.id} changeScore={props.changeScore} />
-    </div>
-)
+                <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore} />
+            </div>
+        )
+    }
+}
+
+// export const Player = (props) => {
+//     console.log(this.props.name, ' rendered');
+//
+//     return(
+//         <div className="player">
+//         <span className="player-name">
+//             <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>X</button>
+//             {this.props.name}
+//         </span>
+//             <Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore} />
+//         </div>
+//     )
+// }
